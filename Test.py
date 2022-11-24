ab26425a6b7f80ce8a944ed706b03c69b0bf4d07
Akhil Tavva <95878957+Akhil-Tavva@users.noreply.github.com>
2022-11-25 00:09:02 +0530

    Add files via upload

--- Test.py
+++ Test.py
@@ -7,17 +7,17 @@ class TestGCD(unittest.TestCase):
         result1 = gcd(8,4)
         self.assertEqual(result1,4)
     def test_gcd2(self):
-        result1 = gcd(8,4)
-        self.assertEqual(result1,4)
+        result1 = gcd(12,24)
+        self.assertEqual(result1,12)
     def test_gcd3(self):
-        result1 = gcd(8,4)
+        result1 = gcd(16,36)
         self.assertEqual(result1,4)
     def test_gcd4(self):
-        result1 = gcd(8,4)
-        self.assertEqual(result1,4)
+        result1 = gcd(10,5)
+        self.assertEqual(result1,10)
     def test_gcd5(self):
-        result1 = gcd(8,4)
-        self.assertEqual(result1,4)
+        result1 = gcd(15,25)
+        self.assertEqual(result1,10)
 
 if __name__ == "__main__":
     unittest.main()
